4dd0edf91cde1204ad4587dd3dd1817ed39baaa9
alexisweeren <alexis.weeren@gmail.com>
2024-08-07 16:08:01 -0700

    added await

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -4,7 +4,7 @@ import { loadStripe } from '@stripe/stripe-js'
 import { StripeContext } from '@/context/stripeContext'
 import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe-js'
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+const stripePromise = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
 export default function CheckOut() {
 
