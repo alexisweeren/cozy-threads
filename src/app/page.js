a6ff61226d5e5fabe812115b43c66176d14f0451
alexisweeren <alexis.weeren@gmail.com>
2024-08-07 13:36:20 -0700

    fixed error in updated import

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import Product from '../../src/context/productContext.js'
+import Product from '@/components/products.js'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
